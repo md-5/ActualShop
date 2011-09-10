d16f5efd553d078db7efaeada9305e2a43fff445
md-5 <md_5@bigpond.com>
2011-09-10 15:30:57 +1000

    Init push 1

---
+++ src/main/java/com/md5/actualshop/ActualShop.java
@@ -0,0 +1,13 @@
+package com.md5.actualshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class ActualShop 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
