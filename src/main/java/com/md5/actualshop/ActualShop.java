97293856bf8fdaebe5b0ad4fcac0b281bceec764
md-5 <md_5@bigpond.com>
2011-09-10 15:57:26 +1000

    Commit

--- src/main/java/com/md5/actualshop/ActualShop.java
+++ src/main/java/com/md5/actualshop/ActualShop.java
@@ -1,13 +1,18 @@
 package com.md5.actualshop;
 
-/**
- * Hello world!
- *
- */
-public class ActualShop 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+ 
+public class ActualShop extends JavaPlugin {
+ 
+	Logger logger = Bukkit.getServer().getLogger();
+ 
+	public void onEnable(){
+		logger.info("ActualShop has been enabled!");
+	}
+ 
+	public void onDisable(){
+		logger.info("ActualShop has been disabled.");
+	}
 }
